ccd8505ce3d5360dbdec21aaa059a6aeee0c14fe
BommisettyTejaswa <tejaswachinnu@gmail.com>
2026-06-09 16:32:31 +0530

    Add files via upload

---
+++ Day-11/Sql_intro.sql
@@ -0,0 +1,20 @@
+
+-- USE college;
+-- CREATE TABLE Student(
+--     Student_roll VARCHAR(20),
+--     Student_name VARCHAR(20),
+--     Year_of_study INT
+-- );
+-- SHOW TABLES;
+-- INSERT INTO Student VALUES ('S101','Ravi',1);
+-- SELECT * FROM Student;
+-- INSERT INTO Student VALUES ('S101','Ravi',1);
+
+-- SELECT * FROM Student;
+-- SHOW TABLES;
+INSERT INTO Student
+VALUES
+('S102','Priya',2),
+('S103','Arjun',3),
+('S104','Sneha',4);
+SELECT * FROM Student;
